66a38464e26725eac561998e38d209c47d0cf616
Gabriel Felicidade <gabriel.felicidade@etec.sp.gov.br>
2021-04-15 22:34:18 -0300

    merge new structure

--- src/main/kotlin/Codeforces/4A/4A.kt
+++
@@ -1,9 +0,0 @@
-import java.util.*
-
-fun main(args: Array<String>) {
-    val sc = Scanner(System.`in`);
-
-    val x = sc.nextInt()
-
-    println(if(x%2 == 0 && x != 2) "YES" else "NO")
-}
--- src/main/kotlin/URI/1153/1153.kt
+++
@@ -1,9 +0,0 @@
-import java.util.*
-
-fun main(args: Array<String>) {
-    val sc = Scanner(System.`in`);
-
-    val n = sc.nextInt()
-
-    println((n downTo 1).reduce { acc, i -> acc * i })
-}
