9ce61adb57c0bb00387d49777d3d4beb599239cb
Gabriel Felicidade <gabriel.felicidade@etec.sp.gov.br>
2022-06-01 18:40:18 -0300

    merge

--- beecrowd/1260/Main.java
+++ beecrowd/1260/Main.java
@@ -17,7 +17,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        
         int t = Integer.parseInt(br.readLine());
 
         br.readLine();
